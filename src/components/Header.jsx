0c271c1f57eaee0f4841008f8603cd9382ec9c74
NasrAllah <shaer.nasrallah@gmail.com>
2024-05-04 22:35:14 +0300

    Assets & Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,88 @@
+import {brainwave} from "../assets/index.js";
+import {navigation} from "../constants/index.js";
+import {useLocation} from "react-router-dom";
+import Button from "./Button.jsx";
+import {HamburgerMenu} from "./design/Header.jsx";
+import {useState} from "react";
+import MenuSvg from "../assets/svg/MenuSvg.jsx";
+import {disablePageScroll , enablePageScroll } from "scroll-lock";
+
+const Header = () => {
+    const pathname = useLocation();
+    const [openNav, setOpenNav] = useState(false);
+
+    const handleOpenNavbar = () => {
+        if(openNav){
+            setOpenNav(false);
+            enablePageScroll();
+        }else {
+            setOpenNav(true);
+            disablePageScroll();
+        }
+    }
+    const handleOnClick = () => {
+        if(!openNav) return;
+        enablePageScroll();
+        setOpenNav(false);
+
+
+    }
+
+    return (
+        <div className={`fixed top-0 z-50 bg-n-8/90
+         left-0 border-b border-n-6  w-full
+        ${openNav ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'}
+        `}>
+            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+                <a className="block w-[12rem] xl:mr-8" href="#hero">
+                    <img
+                        src={brainwave}
+                        alt="Brainwave logo"
+                        width={190}
+                        height={40}
+                    />
+                </a>
+                <nav className={`fixed top-[5rem] left-0 right-0
+                 bottom-0 lg:static lg:flex lg:mx-auto g:bg-transparent
+                 ${openNav ? 'flex' : 'hidden' }
+                 `}>
+                    <div className="relative m-auto z-2 flex flex-col lg:flex-row items-center justify-center">
+                        {navigation.map(link => (
+                            <a  onClick={handleOnClick}
+                                href={link.url}
+                                key={link.id}
+                                className={`block relative uppercase font-code text-2xl lg:text-xs lg:font-semibold
+                                text-n-1 hover:text-color-1 transition-colors px-6 py-6 md:py-8 lg:-mr-0.25 xl:px-12
+                                lg:hover:text-n-1 lg:leading-5
+                                ${link.onlyMobile && 'lg:hidden'} 
+                                ${pathname.hash === link.url  ?  'z-2 lg:text-n-1' : 'lg:text-n-1/50' }
+                                `}
+                            >
+                                {link.title}
+                            </a>
+                        ))}
+
+
+                    </div>
+                    <HamburgerMenu />
+                </nav>
+
+                <a href="#signup"
+                    className="hidden lg:block button mr-8 text-n-1/50 hover:text-n-1 transition-colors">
+                    New Account
+                </a>
+
+                <Button className="hidden lg:flex">
+                    Sign In
+                </Button>
+
+                <Button className="ml-auto lg:hidden" px="px-3" onClick={handleOpenNavbar}>
+                    <MenuSvg openNavigation={openNav}/>
+                </Button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
